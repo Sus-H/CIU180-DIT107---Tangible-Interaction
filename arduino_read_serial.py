ea71939f85d97a28e147a7723471c29523c253f6
Sus-H <susselelle@hotmail.com>
2024-02-22 11:51:58 +0100

    actually working code

--- arduino_read_serial.py
+++ arduino_read_serial.py
@@ -5,7 +5,8 @@ from pathlib import Path
 def readserial(comport, baudrate):
 
     ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
-    current_path = Path().cwd()
+    current_path = str(Path().cwd())
+    # print(current_path)
 
     while True:
         data = ser.readline().decode().strip()
@@ -14,11 +15,17 @@ def readserial(comport, baudrate):
             print(data)
     
         if data == "tone1":
-            audio = current_path / "sneeze.wav"
+            audio = current_path + "\sneeze.wav"
             playsound(audio)
         elif data == "tone2":
-            audio = current_path / "failpiano.wav"
+            audio = current_path + "\pianofail.wav"
             playsound(audio)
+        elif data == "noTone":
+            print("Silence...")
+
             
 
 fromSerial = readserial("COM11", 9600)
+
+# audio = str(Path().cwd()) + "\sneeze.wav"
+# playsound(audio)
