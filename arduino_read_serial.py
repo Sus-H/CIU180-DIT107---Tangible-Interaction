7f7eea8e32a6d0c06ce569898f2d33fe951859f4
Sus-H <susselelle@hotmail.com>
2024-02-22 10:00:44 +0100

    python serial read

---
+++ arduino_read_serial.py
@@ -0,0 +1,18 @@
+import serial
+
+
+def readserial(comport, baudrate):
+
+    ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
+
+    while True:
+        data = ser.readline().decode().strip()
+        if data:
+            print(data)
+
+
+# if __name__ == '__main__':
+
+#     readserial('COM5', 9600)
+
+print(readserial('COM5', 9600))
