7b4f64129ebd803b496b59b8a796c781b022ad97
Sus-H <susselelle@hotmail.com>
2024-02-23 16:45:40 +0100

    working as fk version 1

--- arduino_read_serial.py
+++ arduino_read_serial.py
@@ -1,32 +1,38 @@
 import serial
 import serial.tools.list_ports
-import scamp # http://scamp.marcevanstein.com/narrative/tutorial_videos.html
+import scamp  # http://scamp.marcevanstein.com/narrative/tutorial_videos.html
 
 # from pathlib import Path
 # current_path = str(Path().cwd())
 
 # Get information from arduinos serial port.
-def readserial(comport, baudrate):
-    ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
+def readserial(comport, baudrate) -> serial:
+    return serial.Serial(comport, baudrate, timeout=0.1)  # 1/timeout is the frequency at which the port is read
 
-    while True:
-        data = ser.readline().decode().strip()
+# play_note(midi pitch value, volume 0-1, duration in seconds)
+# 60 is middle C
+# if data == "tone1":
+#     piano.play_note(60, 0.8, 1) 
+#     # continue
+# elif data == "tone2":
+#     piano.play_note(66, 0.8, 1)
 
-        if data:
-            print(data)
+# elif data == "tone3":
+#     piano.play_note(50, 0.8, 1)
+#     # continue
+# elif data == "noTone":
+#     # print(data)
+#     print("Silence...")
+#     # continue
 
-        # play_note(midi pitch value, volume 0-1, duration in seconds)
-        # 60 is middle C
-        if data == "tone1":
-            piano.play_note(60, 0.8, 1) 
-            # continue
-        elif data == "tone2":
-            piano.play_note(66, 0.8, 1)
-            # continue
-        elif data == "noTone":
-            # print(data)
-            print("Silence...")
-            # continue
+# def pickInstrument() -> None:
+#     pass
+
+# def pickInstrument() -> None:
+#     return None
+
+
+# values_arduino = [play_tone, volume/100, instrument, durations, drum_sound, touchpad]
 
 # Automatically get the correct port
 ports = list(serial.tools.list_ports.comports())
@@ -38,10 +44,33 @@ for p in ports:
         print(port)
 
 # Create a new "Music session" and use run as server to suppress the error messages 
+
 s = scamp.Session()
-# s = scamp.Session.run_as_server(self=s)
-piano = s.new_part("piano")
-piano.play_note(65, 1, 1)
+s = scamp.Session.run_as_server(self=s)  #COMMENT OUT IF TESTING
+# s.print_default_soundfont_presets()
+piano = s.new_part("Piano Merlin")
+# piano.play_note(65, 1, 1)
+# drums = s.new_part("Music Box")
+# for i in tones_MIDI:
+#     drums.play_note(i, 1, 1)
+
+
 
 # Use function to get information from arduinos serial port.
-# fromSerial = readserial(port, 9600)
+ser = readserial(port, 9600)
+while True:
+    data = ser.readline().decode().strip()
+    if data:
+        # print(data)
+
+        data_converted = str(data).split()
+        print(data_converted)
+        play_tone = float(data_converted[0])
+        volume = float(data_converted[1])
+        instrument = float(data_converted[2])
+        durations = float(data_converted[3])
+        drum_sound = float(data_converted[4])
+        touchpad = float(data_converted[5])
+
+        piano.play_note(play_tone, volume/100, durations)
+
---
+++ hardware_configurations/hardware_configurations.ino
@@ -0,0 +1,138 @@
+/*
+  Use an ultrasensor to read distance.
+  Then depending on the distance, lighting a lamp and making a buzzer sound.
+*/
+// Tone library?
+#include <Arduino.h>
+#define TONE_USE_INT
+#define TONE_PITCH 440
+#include <TonePitch.h>
+
+
+#define LED_R_PIN 13 // red LED pin
+#define LED_B1_PIN 12 // blue LED pin
+#define LED_W_PIN 11 // white LED pin
+#define LED_B2_PIN 10 // second blue LED pin
+
+#define TOUCH_PIN 7 // Touchpad
+
+#define KNOB_0_PIN A0 // KNOB pin
+#define KNOB_1_PIN A1 // KNOB tiny pin
+#define KNOB_2_PIN A2 // KNOB tiny pin
+
+#define SLIDER_5_PIN A5 // SLIDER pin
+
+// ULTRASENSOR pins
+const int trigPin = 5;
+const int echoPin = 6;
+
+int duration = 0;
+float distance = 0;
+
+// POTENTIOMETERS
+int knob_0_value = 0;
+int knob_1_value = 0;
+int knob_2_value = 0;
+int slider_5_value = 0;
+
+int values[6];
+
+void setup() {
+  // ULTRASENSOR
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, INPUT);
+
+  // TOUCHPAD
+  pinMode(TOUCH_PIN, INPUT);
+
+  // LED
+  pinMode(LED_R_PIN, OUTPUT);
+  pinMode(LED_B1_PIN, OUTPUT);
+  pinMode(LED_W_PIN, OUTPUT);
+  pinMode(LED_B2_PIN, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  digitalWrite(LED_R_PIN, LOW); 
+  digitalWrite(LED_B1_PIN, LOW); 
+  digitalWrite(LED_W_PIN, LOW); 
+  digitalWrite(LED_B2_PIN, LOW); 
+  
+  values[0] = play_tone();
+  values[1] = volume();
+  values[2] = instrument();
+  values[3] = durations();
+  values[4] = drum_sound();
+  values[5] = touchpad();
+
+  for (int i = 0; i < 6; i++) {
+    Serial.print(values[i]);
+    Serial.print(" ");
+  }
+  Serial.println("");
+  delay(500);
+}
+
+
+// Functions for the different components
+// Ultra sonic sensor - different distance = different tone.
+int play_tone() {
+  
+  // Send sonic waves, delay for time to send and read
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration * .0343) / 2;
+  
+  // up to 35 cm
+  if (distance > 35) {
+    return 0;
+  }
+  return map(distance, 0, 35, 60, 66);
+}
+
+// POTENTIOMETERS Values 0-1023
+float volume() {
+  slider_5_value = analogRead(SLIDER_5_PIN);
+  return map(slider_5_value, 0, 1023, 0, 100);
+}
+
+
+int instrument() {
+  knob_0_value = analogRead(KNOB_0_PIN);
+  return map(knob_0_value, 0, 1023, 1, 5);
+}
+
+
+int durations() {
+  knob_1_value = analogRead(KNOB_1_PIN);
+  return map(knob_1_value, 0, 1023, 1, 5);
+}
+
+
+int drum_sound() {
+  knob_2_value = analogRead(KNOB_2_PIN);
+  return map(knob_2_value, 0, 1023, 1, 3);
+}
+
+
+int touchpad() {
+  return digitalRead(TOUCH_PIN);
+}
+
+
+void webcam_module() {
+
+}
+
+
+void touchpad_LCD_module() {
+
+}
